ef6ca862b67d2cc945635d1a7593e76d4f60924c
mbarela82 <mbarela82@gmail.com>
2023-11-14 17:22:44 -0700

    Initial commit

---
+++ backend/controllers/userController.js
@@ -0,0 +1,12 @@
+import asynHandler from 'express-async-handler';
+
+// @desc Auth user/set token
+// route POST /api/users/auth
+// @access Public
+const authUser = asynHandler(async (req, res) => {
+    res.status(200).json({message: 'Auth User'});
+});
+
+export {
+    authUser,
+};
